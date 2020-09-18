cf8654c98ad07f186043262452706244d6b21cf8
husein <alhussein salam>
2020-09-18 06:59:55 +0300

    added 18.7 TowerOfHanoi

--- src/main/java/ch18/recursion/TowerOfHanoi.java
+++ src/main/java/ch18/recursion/TowerOfHanoi.java
@@ -1,4 +1,23 @@
 package ch18.recursion;
 
 public class TowerOfHanoi {
+    static final char DISK_A = 'A';
+    static final char DISK_B = 'B';
+    static final char DISK_C = 'C';
+
+    public static void main(String[] args) {
+        moveDisks(10, DISK_A, DISK_B, DISK_C);
+    }
+
+    public static void moveDisks (int n, char fromTower, char toTower, char auxTower) {
+        if (n == 1) {
+            System.out.println("Moving disk " + n + " from " + fromTower + " to " + toTower);
+        } else {
+            moveDisks(n-1, fromTower, auxTower, toTower);
+            System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
+            moveDisks(n-1, auxTower, toTower, fromTower);
+        }
+    }
+
+
 }
