113e106649c2205d8b8fa72f3d1ea3930643c724
husein <alhussein salam>
2020-09-18 11:22:11 +0300

    added CH20 and CH21

---
+++ src/main/java/ch20/data_structure/JavaDataStructures.java
@@ -0,0 +1,25 @@
+package ch20.data_structure;
+
+public class JavaDataStructures {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Data structures in java
+     * Set: store a group of nonduplicate elements
+     * List: store ordered collection of elements
+     * Stack: last-in first-out
+     * Queue: first-in first-out
+     * PriorityQueue: store elements according to their priority using the comparable interface
+     */
+
+    /**
+     * Lists
+     * ArrayList
+     * LinkedList ==> can order elements, better for adding and removing elements from the beginning and the end of the list
+     * Vector: same as ArrayList but it contains synchronized methods so it's threading safe
+     */
+
+
+}
---
+++ src/main/java/ch21/sets_maps/SetsAndMaps.java
@@ -0,0 +1,10 @@
+package ch21.sets_maps;
+
+public class SetsAndMaps {
+    /**
+     * Set: store elements with nonduplication rule
+     * HashSet
+     * LinkedHashSet: same as HashSet but allows to order elements
+     * TreeSet: faster for search
+     */
+}
