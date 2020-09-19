3636536869201202486cf96d660731a828cb88c6
husein <alhussein salam>
2020-09-19 20:56:43 +0300

    added ch23

---
+++ src/main/java/ch23/sorting/InsertionSort.java
@@ -0,0 +1,13 @@
+package ch23.sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sortAsc(int[] originalArray) {
+        for (int i = 0; i < originalArray.length; i++) {
+
+        }
+    }
+}
